6aaaf05f89b18f726ec6ddc017e2b10762ce8102
Ian Yin <yin.ian093@gmail.com>
2020-08-22 11:25:51 +1000

    reset

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import Header from './component/Header';
-import Footer from './component/Footer';
-import Form from './component/Form';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Form />
-      <Footer />
+
     </div>
   );
 }
