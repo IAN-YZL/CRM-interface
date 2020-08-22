a70c71ed4e0ca0a0e1704256545f96c4754d6c79
Ian Yin <yin.ian093@gmail.com>
2020-08-22 11:16:34 +1000

    create react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Form from './component/Form';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Form/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const availableTime = this.getAvailableTime();
+
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+};
+
+export default Form;
