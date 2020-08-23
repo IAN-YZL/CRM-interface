5c7c6ae1858aad82021f2d71a4799e4d7f996556
Ian Yin <yin.ian093@gmail.com>
2020-08-23 16:13:51 +1000

    create filter

---
+++ src/component/Category/component/Filters/Filters.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ReactComponent as Icon} from './assets/svg/filter.svg';
+
+const Filters = () => (
+    <div>
+        <div>
+            <Icon />
+        </div>
+        <p>Filter</p>
+    </div>
+);
+
+export default Filters;
