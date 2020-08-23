b710d3a1658a3b2fb6d397b5d320bf6e60d37309
Ian Yin <yin.ian093@gmail.com>
2020-08-23 16:55:09 +1000

    complete the style of filter

--- src/component/Category/Header/component/Filters/Filters.jsx
+++ src/component/Category/Header/component/Filters/Filters.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {ReactComponent as Icon} from './assets/svg/filter.svg';
+import {ReactComponent as FilterIcon} from './assets/svg/filter.svg';
+import {ReactComponent as DropIcon} from './assets/svg/drop.svg';
+import style from './Filters.module.scss';
 
 const Filters = () => (
-    <div>
-        <div>
-            <Icon />
+    <div className={style.filter}>
+        <div className={style.filter_icon}>
+            <FilterIcon />
         </div>
         <p>Filter</p>
+        <div className={style.drop_icon}>
+            <DropIcon />
+        </div>
     </div>
 );
 
