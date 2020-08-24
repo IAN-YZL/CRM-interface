aba3bf20d86d6210cf0bcb8341a755904db53fdc
Ian Yin <yin.ian093@gmail.com>
2020-08-24 16:43:50 +1000

    add banner

--- src/component/Content/component/Banner/Banner.jsx
+++ src/component/Content/component/Banner/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Ticket from '../../../Category/Plan/component/Ticket';
 import SettingButton from './component/SettingButton';
+import NewModule from './component/NewModule';
 
 const Banner = () => (
     <div>
         <Ticket content="Hi" project="Leadership" />
         <SettingButton />
+        <NewModule />
     </div>
 );
 
---
+++ src/component/Content/component/Banner/component/NewModule.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from '../Banner.module.scss';
+
+const NewModule = () => (
+    <div>
+        <button className={style.newModule}>+&nbsp; New Module</button>
+    </div>
+);
+
+export default NewModule;
