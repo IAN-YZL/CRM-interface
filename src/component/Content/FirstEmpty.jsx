991bb8c2787def91c9fed57a083a2701db4adcee
Ian Yin <yin.ian093@gmail.com>
2020-08-31 16:45:06 +1000

    Add new empty page

---
+++ src/component/Content/FirstEmpty.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FirstEmpty = () => (
+    <div>First Empty Page</div>
+);
+
+export default FirstEmpty;
---
+++ src/component/Content/SecondEmpty.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SecondEmpty = () => (
+    <div>First Empty Page</div>
+);
+
+export default SecondEmpty;
